be20ef40942c522aaa2168e0f4eaa08cae64ad1a
CarlosPantin~ <carlospantin@live.com.mx>
2023-09-22 14:03:29 +0300

    Changes to be committed: Header and About section new file:   src/components/About.css modified:   src/components/About.js modified:   src/components/Header.css modified:   src/components/Header.js new file:   src/images/MyPicture-modified.png new file:   src/images/MyPicture.png

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,58 @@
-// About.js
-import React from 'react';
+import React from "react";
+import "./About.css";
+import mypicture from "../images/MyPicture-modified.png";
 
 const About = () => {
   return (
     <section id="about" className="about-section">
       <div className="about-content">
-        <h2>About Me</h2>
-        <p>
-          Hello, I'm [Your Name]. I'm a passionate front-end developer with a love for creating
-          interactive and user-friendly web applications. I enjoy working with
-          technologies like React, HTML, CSS, and JavaScript to build beautiful and
-          responsive websites.
-        </p>
-        <p>
-          When I'm not coding, I love to [mention your hobbies or interests].
-          Feel free to explore my portfolio to see some of my recent projects.
-        </p>
+        <div className="row">
+          <div className="col-1"></div>
+          <div className="col-2">
+            <img src={mypicture} alt="Your Name" className="about-image" />
+          </div>
+          <div className="col-3">
+            <h2>About Me</h2>
+
+            <p>
+              Hello, I'm Carlos Pantin. I'm a passionate developer with a love
+              for creating interactive and user-friendly web applications. I
+              enjoy working with technologies like React, HTML, CSS, Javascript
+              among others to build beautiful and responsive websites.
+            </p>
+            <p>
+              When I'm not coding, I love to learn more about new coding
+              practices and I love to keep a healthy body with working out. Feel
+              free to explore my portfolio to see some of my recent projects.
+            </p>
+          </div>
+          <div className="col-4"></div>
+        </div>
+        <h2 className="sys">Skills and strengths</h2>
+        <div className="row">
+          <div className="col-1">
+            <div className="skill-card">
+              <h3>Front-End Development</h3>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="skill-card">
+              <h3>Front-End Development</h3>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,32 @@
 // Header.js
-import React from 'react';
-import './Header.css'; 
-import ParticlesBg from 'particles-bg';
-
-
+import React from "react";
+import "./Header.css";
+import ParticlesBg from "particles-bg";
 
 const Header = () => {
   return (
     <header className="header-container">
-      <ParticlesBg type="square" num={15}  bg={true}/>
+      <ParticlesBg type="square" num={15} bg={true} />
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+        </ul>
+      </nav>
       <div className="header-content">
-        <nav>
-          <ul>
-            <li><a href="#about">About</a></li>
-            <li><a href="#portfolio">Portfolio</a></li>
-          </ul>
-        </nav>
-        <h1>Your Name</h1>
-        <p>Front-End Developer</p>
+        <h1>Carlos Pantin</h1>
+        <p>
+          Passionate Junior Developer. I love web development using React,
+          however I am well acquainted wiht other technologies and approaches. I
+          love challenges and I love to learn as much as I can to adapt to the
+          industry changes. Feel free to see a little bit more about me and my work down below. (More projects WIP)
+        </p>
         <a
-          href="https://github.com/your-github-username"
+          href="https://github.com/CarlosPantin"
           target="_blank"
           rel="noopener noreferrer"
           className="github-button"
