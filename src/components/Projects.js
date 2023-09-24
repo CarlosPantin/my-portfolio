c0e117e646a745dd790de7666f0f66d8895c5f6d
CarlosPantin~ <carlospantin@live.com.mx>
2023-09-24 18:11:35 +0300

    Changes to be committed: modified:   src/components/Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,11 +26,12 @@ const Projects = () => {
       status: "https://www.youtube.com/watch?v=xsWV8AcXar8",
       github: "https://github.com/CarlosPantin/spotify-app",
     },
+    
   ];
 
   return (
     <section id="projects" className="projects-section">
-      <h2 className="section-title">Projects</h2>
+      <h2 className="section-title">Projects (more in development)</h2>
       <div className="projects-container">
         {projects.map((project, index) => (
           <div key={index} className="project-card">
